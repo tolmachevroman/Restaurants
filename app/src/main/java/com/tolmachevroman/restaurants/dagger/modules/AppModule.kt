875e3642726fcdd7415abeff9426a849b6ed2042
tolmachevroman <tolmachevroman@gmail.com>
2017-11-22 01:53:32 -0300

    RestaurantsDao added

--- app/src/main/java/com/tolmachevroman/restaurants/dagger/modules/AppModule.kt
+++ app/src/main/java/com/tolmachevroman/restaurants/dagger/modules/AppModule.kt
@@ -3,6 +3,7 @@ package com.tolmachevroman.restaurants.dagger.modules
 import android.app.Application
 import android.arch.persistence.room.Room
 import com.tolmachevroman.restaurants.models.Database
+import com.tolmachevroman.restaurants.models.restaurants.RestaurantsDao
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -21,4 +22,8 @@ class AppModule(private val app: Application) {
     @Singleton
     fun provideRestaurantsDatabase(app: Application): Database =
             Room.databaseBuilder(app, Database::class.java, "restaurants_db").build()
+
+    @Provides
+    @Singleton
+    fun provideRestaurantsDao(db: Database): RestaurantsDao = db.restaurantsDao()
 }
--- app/src/main/java/com/tolmachevroman/restaurants/models/Database.kt
+++ app/src/main/java/com/tolmachevroman/restaurants/models/Database.kt
@@ -3,11 +3,12 @@ package com.tolmachevroman.restaurants.models
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.tolmachevroman.restaurants.models.restaurants.Restaurant
+import com.tolmachevroman.restaurants.models.restaurants.RestaurantsDao
 
 /**
  * Created by romantolmachev on 22/11/2017.
  */
 @Database(entities = arrayOf(Restaurant::class), version = 1)
 abstract class Database : RoomDatabase() {
-
+    abstract fun restaurantsDao(): RestaurantsDao
 }
