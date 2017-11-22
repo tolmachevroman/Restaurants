7b9c17c2f074f07b0dcbb0721448b2aee8b9324e
tolmachevroman <tolmachevroman@gmail.com>
2017-11-22 02:21:11 -0300

    Provide WebService added to Dagger

--- app/src/main/java/com/tolmachevroman/restaurants/dagger/modules/NetModule.kt
+++ app/src/main/java/com/tolmachevroman/restaurants/dagger/modules/NetModule.kt
@@ -2,6 +2,7 @@ package com.tolmachevroman.restaurants.dagger.modules
 
 import android.app.Application
 import com.tolmachevroman.restaurants.BuildConfig
+import com.tolmachevroman.restaurants.datasources.webservice.WebService
 import dagger.Module
 import dagger.Provides
 import okhttp3.Cache
@@ -21,6 +22,21 @@ class NetModule(private val baseUrl: String) {
 
     @Provides
     @Singleton
+    fun provideWebService(retrofit: Retrofit): WebService =
+            retrofit.create<WebService>(WebService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create())
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .build()
+    }
+
+    @Provides
+    @Singleton
     fun provideHttpCache(app: Application): Cache {
         val cacheSize = 10 * 1024 * 1024L
         return Cache(app.cacheDir, cacheSize)
@@ -32,16 +48,6 @@ class NetModule(private val baseUrl: String) {
         val client = OkHttpClient.Builder().addInterceptor(interceptor)
         client.cache(cache)
         return client.build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-        return Retrofit.Builder()
-                .addConverterFactory(MoshiConverterFactory.create())
-                .baseUrl(baseUrl)
-                .client(okHttpClient)
-                .build()
     }
 
     @Provides
