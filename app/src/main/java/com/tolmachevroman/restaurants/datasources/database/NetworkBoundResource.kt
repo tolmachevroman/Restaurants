bc84d166de23f3db6ca2a56d3bfe5b67809cc2b3
tolmachevroman <tolmachevroman@gmail.com>
2017-11-23 13:14:41 -0300

    Catch IOException in network call

--- app/src/main/java/com/tolmachevroman/restaurants/datasources/database/NetworkBoundResource.kt
+++ app/src/main/java/com/tolmachevroman/restaurants/datasources/database/NetworkBoundResource.kt
@@ -4,10 +4,12 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MediatorLiveData
 import android.support.annotation.MainThread
 import android.support.annotation.WorkerThread
+import com.tolmachevroman.restaurants.BuildConfig
 import com.tolmachevroman.restaurants.datasources.webservice.Error
 import com.tolmachevroman.restaurants.datasources.webservice.Resource
 import com.tolmachevroman.restaurants.utils.AppExecutors
 import retrofit2.Call
+import java.io.IOException
 
 /**
  * Created by romantolmachev on 22/11/2017.
@@ -33,32 +35,36 @@ abstract class NetworkBoundResource<T>(private val appExecutors: AppExecutors) {
 
         appExecutors.networkIO().execute {
 
-            val response = createNetworkCall().execute()
+            try {
+                val response = createNetworkCall().execute()
 
-            println("response is: $response")
+                println("response is: $response")
 
-            when (response.isSuccessful) {
-                true -> appExecutors.diskIO().execute {
-                    saveNetworkCallResult(response.body())
+                when (response.isSuccessful) {
+                    true -> appExecutors.diskIO().execute {
+                        saveNetworkCallResult(response.body())
 
-                    appExecutors.mainThread().execute {
-                        val newDbSource = loadFromDatabase()
-                        result.addSource(newDbSource) { newData ->
-                            result.removeSource(newDbSource)
-                            result.setValue(Resource.success(newData))
+                        appExecutors.mainThread().execute {
+                            val newDbSource = loadFromDatabase()
+                            result.addSource(newDbSource) { newData ->
+                                result.removeSource(newDbSource)
+                                result.setValue(Resource.success(newData))
+                            }
                         }
                     }
-                }
 
-                false -> appExecutors.mainThread().execute {
-                    result.addSource(dbSource) { newData -> result.setValue(Resource.error(newData, Error(response.code(), response.message()))) }
-                }
+                    false -> appExecutors.mainThread().execute {
+                        result.addSource(dbSource) { newData -> result.setValue(Resource.error(newData, Error(response.code(), response.message()))) }
+                    }
 
+                }
+            } catch (exc: IOException) {
+                System.err.println("Make sure your server ${BuildConfig.URL} is running.")
+                appExecutors.mainThread().execute {
+                    result.addSource(dbSource) { newData -> result.setValue(Resource.error(newData, Error(503, "Service Unavailable."))) }
+                }
             }
-
         }
-
-
     }
 
     fun asLiveData(): LiveData<Resource<T>> = result
